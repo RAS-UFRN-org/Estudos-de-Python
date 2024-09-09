77dd316a778082a19e11202ee26df00658a117c3
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-09-09 18:31:55 -0300

    fix q8

--- 03-array-strings-funções/script-08.py
+++ 03-array-strings-funções/script-08.py
@@ -1,11 +1,8 @@
-lista1 = [2,3,4,6,7,8,9,-10,2]
-lista_de_somas = []
+lista1 = [2,-3,3,4]
 soma_total = -100000000
-
 for i in range(len(lista1)):
     for j in range(i, len(lista1)):
         soma_atual = sum(lista1[i:j+1])
-        lista_de_somas.append(soma_atual)
         if soma_atual > soma_total:
             soma_total = soma_atual
             
